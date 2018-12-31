1a3403e9bf3cbdbda0f902d54c1c58ce0e78e225
kostyahoriainov <konstiantyn.horiainov@gmail.com>
2018-12-31 22:31:01 +0200

    'users_reducer'

---
+++ src/reducer/__tests__/users.spec.js
@@ -0,0 +1,23 @@
+import users from '../users'
+import { getUsers, usersDidLoad } from '../../action';
+
+describe("users reducer", () => {
+    it("should store users in the state", () => {
+        const user = {
+            name: 'Alex',
+            email: 'Alex@gmail.com',
+            age: 30
+        }
+        const newState = users(undefined, getUsers(user));
+        console.log(newState)
+        expect(newState.users.name).toEqual('Alex');
+        expect(newState.users.email).toEqual('Alex@gmail.com');
+        expect(newState.users.age).toEqual(30);
+        expect(newState.isLoaded).toEqual(false);
+    })
+
+    it("should store isLoaded in the state", () => {
+        const newState = users(undefined, usersDidLoad());
+        expect(newState.isLoaded).toEqual(true)
+    })
+})
